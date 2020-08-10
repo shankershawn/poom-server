36984b3446f9054b3a62aaf79f7ecb6d84d1c6b2
unknown <shankershawn@gmail.com>
2020-08-10 18:33:12 +0100

    Changes for https://sganai.atlassian.net/browse/PPS-13

--- router/login.route.js
+++ router/login.route.js
@@ -1,11 +1,10 @@
 module.exports = (async () => {
     const express = require('express');
     const jwtUtil = require('../util/jwt.util');
+    const ssoUtil = await require('../util/sso.util');
     const login = express();
     const bodyParser = require('body-parser');
-    const poomdb = await require('../util/mongodb.util');
     const modelUtil = require('../util/model.util');
-    const messages = [];
     const cryptojs = require('crypto-js');
     
     console.log("Loading Login route");
@@ -14,7 +13,7 @@ module.exports = (async () => {
     login.use(bodyParser.urlencoded({extended: false}));
     login.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", process.env.UI_URL);
-        res.header("Access-Control-Allow-Headers", "authorization");
+        res.header("Access-Control-Allow-Headers", "authorization,x-auth-type");
         next();
     });
 
@@ -69,26 +68,66 @@ module.exports = (async () => {
         });
     });
 
+    login.get('/logout', (req, res) => {
+        return new Promise((resolve, reject) => {
+            var token = req.header("authorization");
+            var auth_type = req.header("x-auth-type");
+            token = token.split("Bearer ")[1];
+            if("g" == auth_type){
+                ssoUtil
+                    .revokeGoogleToken(token) 
+                    .then(() => {
+                        res.status(200).send();
+                    })
+                    .catch((err) => {
+                        res.status(400).send({
+                            messageDetail: "Something went wrong. Please try again later."
+                        });
+                    })
+                ;
+            }else if("n" == auth_type){
+                res.status(200).send();
+                /*Code for revoke of JWT for future implementation. Currently not supported.*/
+            }
+        });
+    });
+
     var verifyToken = (req, res) => {
         return new Promise((resolve, reject) => {
             var token = req.header("authorization");
-            token = token.split("Bearer ")[1];
-            if(!token){
+            var auth_type = req.header("x-auth-type");
+            if(!token || !auth_type){
                 reject();
             }else{
-                jwtUtil
-                    .verify(token)
-                    .then((data) => {
-                        if(data.exp - Math.round(Date.now()/1000) < 900){
-                            data.exp = Math.round(Date.now()/1000) + 3600;
-                            setToken(data, res);
-                        }
-                        resolve();
-                    })
-                    .catch((err) => {
-                        reject();
-                    })
-                ;
+                if("g" == auth_type){
+                    token = token.split("Bearer ")[1];
+                    ssoUtil
+                        .verifyGoogleToken(token)
+                        .then(() => {
+                            resolve();
+                        })
+                        .catch((err) => {
+                            reject(err)
+                        })
+                    ;
+                }else if("n" == auth_type){
+                    token = token.split("Bearer ")[1];
+                    jwtUtil
+                        .verify(token)
+                        .then((data) => {
+                            if(data.exp - Math.round(Date.now()/1000) < 900){
+                                data.exp = Math.round(Date.now()/1000) + 3600;
+                                setToken(data, res);
+                            }
+                            resolve();
+                        })
+                        .catch((err) => {
+                            reject(err);
+                        })
+                    ;
+                }else{
+                    reject();
+                }
             }
         });
     };
@@ -98,7 +137,7 @@ module.exports = (async () => {
             .then(() => {
                 res.sendStatus(200);
             })
-            .catch(() => {
+            .catch((err) => {
                 res.status(403).send({
                     messageDetail: "You are not authorized to perform this operation. Please sign-in and try again."
                 });
